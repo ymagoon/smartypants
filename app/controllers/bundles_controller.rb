07b7a815c692beba3eae2088a811599223abb23e
Cassy Myers <cassymyers@gmail.com>
2018-05-16 17:32:15 +0800

    create bundles controller and all 7 routes

---
+++ app/controllers/bundles_controller.rb
@@ -0,0 +1,50 @@
+class BundlesController < ApplicationController
+  before_action :set_bundle, only: [:show, :edit, :update, :destroy]
+  def index
+    @bundles = Bundle.all
+  end
+
+  def show
+  end
+
+  def new
+    @bundle = Bundle.new
+  end
+
+  def create
+    @bundle = Bundle.new(bundle_params)
+    if @bundle.save
+      redirect_to bundle_path(@bundle)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @bundle.update(bundle_params)
+      redirect_to bundle_path(@bundle)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    if @bundle.delete
+      redirect_to bundles_path
+    end
+  end
+
+  private
+
+  def set_bundle
+    @bundle = Bundle.find(params[:id])
+  end
+
+  def bundle_params
+    params.require(:bundle).permit(:name, :gender, :age_group, :price_per_day, :photo)
+  end
+
+end
---
+++ test/controllers/bundles_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class BundlesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
