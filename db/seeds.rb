aa8b71f3099882aea304a53c4ba41777231c8652
Cassy Myers <cassymyers@gmail.com>
2018-05-18 13:29:09 +0800

    here is the css for the banner and the bundles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-a = Bundle.new(name: "Cute Baby Toys", gender: "Female", age_group: "2T", price_per_day: 1, photo: "smarty-pants/li-tzuni-507346-unsplash.jpg", user_id: 1)
+a = Bundle.new(name: "Cute Baby Toys", gender: "Female", age_group: "2T", price_per_day: 1, photo: "smarty-pants/li-tzuni-507346-unsplash.jpg", user_id: 1,  description: "Large Legos, Stuffed Animals, Rattles and more")
 a.save!
-b = Bundle.new(name: "Adorable Girls Clothes", gender: "Female", age_group: "18 to 24 months", price_per_day: 1, photo: "smarty-pants/accessories-adorable-baby-325867.jpg", user_id: 1)
+b = Bundle.new(name: "Adorable Girls Clothes", gender: "Female", age_group: "18 to 24 months", price_per_day: 1, photo: "smarty-pants/accessories-adorable-baby-325867.jpg", user_id: 1,  description: "Pink everything from dresses to blankets to stuffed toys and socks")
 b.save
-c = Bundle.new(name: "Toddler Outfits for play", gender: "Male", age_group: "3T", price_per_day: 1, photo: "smarty-pants/baby-clothes-baby-shoes-booties-47220_1.jpg", user_id: 1)
+c = Bundle.new(name: "Toddler Outfits for play", gender: "Male", age_group: "3T", price_per_day: 1, photo: "smarty-pants/baby-clothes-baby-shoes-booties-47220_1.jpg", user_id: 1,  description: "toddler clothes meant for a sandbox day out or a messy play doe session. Most have a few kid grass-stains, but no holes in the knees and well taken care of. " )
 c.save
